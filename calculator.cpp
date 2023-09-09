1e746ea5597568640c41f1b9af65b9ac2895b9de
CR3op <roldancarlos527@gmail.com>
2023-09-09 18:46:50 -0500

    I've added a number inputs from the user inputs

---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Enter a number: ";
+    cin >> y;
+
+    return 0;
+}
